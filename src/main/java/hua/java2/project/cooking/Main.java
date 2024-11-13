f7d58f24c56fa6ade5d146bbdf745880f69f37c1
kostantis <smavraganiskostantis@gmail.com>
2024-11-13 14:10:07 +0200

    if statement at Main with messages

--- src/main/java/hua/java2/project/cooking/Main.java
+++ src/main/java/hua/java2/project/cooking/Main.java
@@ -1,16 +1,28 @@
 package hua.java2.project.cooking;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
 
         if(args.length == 0) {
             System.out.println("No arguments provided.");
 
-        } else if(args.length == 1) {
+        } else if(args.length == 1 && !args[0].equals("-list")) {
             System.out.println("Print recipe.");
 
-        } else if(args.length >= 2) {
+            //1η λειτουργια
+
+        } else if(args[0].equals("-list") && args.length > 1) {
             System.out.println("Print shopping list.");
+
+            //2η λειτουργια
+
+        } else if(args[0].equals("-list") && args.length == 1) {
+            System.out.println("You should provide a recipe.");
+
+        } else {
+            System.out.println("You should type '-list'.");
 
         }
 
